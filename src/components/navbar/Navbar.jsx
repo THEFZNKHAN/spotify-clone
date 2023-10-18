d88eaed623606d88807f0a70199702126f32bf6a
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2023-10-19 00:49:19 +0530

    Create Navbar 🧭

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,30 +1,33 @@
 import React from "react";
 import {
-  ArrowForwardIosOutlined,
-  ArrowBackIosNewOutlined,
-  PermIdentityOutlined,
-  Downloading,
+    ArrowForwardIosOutlined,
+    ArrowBackIosNewOutlined,
+    PermIdentityOutlined,
+    Downloading,
 } from "@mui/icons-material";
+
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 
 import "./navbar.css";
 
 const Navbar = () => {
-  return (
-    <>
-      <div className="leftSide">
-        <ArrowBackIosNewOutlined />
-        <ArrowForwardIosOutlined />
-      </div>
-      <div className="rightSide">
-        <span>Explore Premium</span>
-        <div>
-          <Downloading />
-          <span>Install App</span>
+    return (
+        <div className="navbar-div">
+            <div className="leftSide">
+                <ArrowBackIosNewOutlined className="icons" />
+                <ArrowForwardIosOutlined className="icons" />
+            </div>
+            <div className="rightSide">
+                <span className="ep-btn">Explore Premium</span>
+                <div className="download-btn">
+                    <Downloading className="icons" />
+                    <span>Install App</span>
+                </div>
+                <NotificationsNoneOutlinedIcon className="icons" />
+                <PermIdentityOutlined className="id-icons" />
+            </div>
         </div>
-        <PermIdentityOutlined />
-      </div>
-    </>
-  );
+    );
 };
 
 export default Navbar;
