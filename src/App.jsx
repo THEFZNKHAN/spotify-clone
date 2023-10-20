1c04a70ac7719ec807d9a8e324cdfcc6ea946520
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2023-10-20 21:38:10 +0530

    Creating sidebar top part

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css';
-import Navbar from './components/navbar/Navbar';
+import Navbar from "./components/navbar/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Sidebar className="sidebar" />
+            <Navbar className="navbar" />
+        </div>
+    );
 }
 
 export default App;
