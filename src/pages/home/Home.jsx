5f21cc24a8e387c5bd770db0f1422131a5e0a910
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2023-10-28 23:01:26 +0530

    Creating 2nd div in Home Page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -54,6 +54,47 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+
+            <div className="div2">
+                <div className="heading">
+                    <h2>Jump back in</h2>
+                </div>
+                <div className="elements">
+                    <div className="ele">
+                        <img
+                            src="https://newjams-images.scdn.co/image/ab67647800003f8a/dt/v3/release-radar/ab6761610000e5eb3bcef85e105dfc42399ef0ba/en"
+                            alt=""
+                        />
+                        <h3>Release Radar</h3>
+                        <h4>Playlist</h4>
+                    </div>
+                    <div className="ele">
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSY_P5OdvcnoSeVpmv0wNCGV3CSbOaj3h7HNp8NVdZ9t2b9pBhi6OrP-ilzHSmOhTUPBls&usqp=CAU"
+                            alt=""
+                            className="img"
+                        />
+                        <h3>Girl in Red</h3>
+                        <h4>Artist</h4>
+                    </div>
+                    <div className="ele">
+                        <img
+                            src="https://i.scdn.co/image/ab67706f00000002c30623cd54c7a127429e4678"
+                            alt=""
+                        />
+                        <h3>Hip Hop Mix</h3>
+                        <h4>Drive slow homie. cover: Big Tymers</h4>
+                    </div>
+                    <div className="ele">
+                        <img
+                            src="https://newjams-images.scdn.co/image/ab67647800003f8a/dt/v3/release-radar/ab6761610000e5eb3bcef85e105dfc42399ef0ba/en"
+                            alt=""
+                        />
+                        <h3>Release Radar</h3>
+                        <h4>Playlist</h4>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
