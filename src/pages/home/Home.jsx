5f058717d58cbcbe4b22b0eec9ebfd5e858cf497
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2023-12-02 19:34:03 +0530

    Basic setup of search Page 🔎

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-import "./home.css";
 import Navbar from "../../components/navbar/Navbar";
+import "./home.css";
 
 const Home = () => {
     const div1Elements = [
--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import Navbar from "../../components/navbar/Navbar";
+import "./search.css";
 
 const Search = () => {
-  return (
-    <div>
-      Search Page
-    </div>
-  )
-}
+    const recentSearches = [
+        {
+            img: "https://i.scdn.co/image/ab6765630000ba8a0e695af6fdf3f35efe2cc4fd",
+            name: "Marvel's Wastelanders",
+            folder: "Marvel & SiriusXM",
+        },
+        {
+            img: "https://i.scdn.co/image/ab6765630000ba8a0e695af6fdf3f35efe2cc4fd",
+            name: "Marvel's Wastelanders",
+            folder: "Marvel & SiriusXM",
+        },
+        {
+            img: "https://i.scdn.co/image/ab6765630000ba8a0e695af6fdf3f35efe2cc4fd",
+            name: "Marvel's Wastelanders",
+            folder: "Marvel & SiriusXM",
+        },
+        {
+            img: "https://i.scdn.co/image/ab6765630000ba8a0e695af6fdf3f35efe2cc4fd",
+            name: "Marvel's Wastelanders",
+            folder: "Marvel & SiriusXM",
+        },
+    ];
 
-export default Search
+    return (
+        <div className="search-div">
+            <Navbar />
+            <div className="recent-div">
+                <h2>Recent searches</h2>
+                <div className="r-elements">
+                    {recentSearches.map((ele) => (
+                        <div className="ele">
+                            <img
+                                src={ele.img}
+                                alt={ele.name}
+                                className={ele.folder === "Artist" ? "img" : ""}
+                            />
+                            <h3>{ele.name}</h3>
+                            <h4>{ele.folder}</h4>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Search;
