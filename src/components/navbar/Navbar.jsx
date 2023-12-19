0bae34a045061afcefaf999bd1b2fdb91bdea8c8
MD FAIZAN KHAN <124388165+THEFZNKHAN@users.noreply.github.com>
2023-12-19 22:55:35 +0530

    Add search functionality to navbar
    * Add search functionality to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,20 +5,36 @@ import {
     PermIdentityOutlined,
     Downloading,
 } from "@mui/icons-material";
-
+import SearchIcon from "@mui/icons-material/Search";
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import { useLocation } from "react-router-dom";
 
 import "./navbar.css";
 
 const Navbar = () => {
+    const location = useLocation();
+
     return (
         <div className="navbar-div">
             <div className="leftSide">
                 <ArrowBackIosNewOutlined className="icons" />
                 <ArrowForwardIosOutlined className="icons" />
+
+                {location.pathname === "/spotify-clone/search" && (
+                    <div className="search">
+                        <SearchIcon />
+                        <input
+                            type="text"
+                            placeholder="What do you want to listen to?"
+                        />
+                    </div>
+                )}
             </div>
+
             <div className="rightSide">
-                <span className="ep-btn">Explore Premium</span>
+                {location.pathname === "/spotify-clone" && (
+                    <span className="ep-btn">Explore Premium</span>
+                )}
                 <div className="download-btn">
                     <Downloading />
                     <span>Install App</span>
